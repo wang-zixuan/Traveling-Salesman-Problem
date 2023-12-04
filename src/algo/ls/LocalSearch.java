2d5081688e29911a8953d7665213f612c4680f74
wang-zixuan <zxwang9811@gmail.com>
2023-12-03 22:05:02 -0500

    Add comments to LocalSearch.java

--- src/algo/ls/LocalSearch.java
+++ src/algo/ls/LocalSearch.java
@@ -7,10 +7,18 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * The LocalSearch class provides methods to find the shortest cycle in a
+ * graph using local search algorithms.
+ */
 public class LocalSearch {
-    private static int minimumCost = Integer.MAX_VALUE;
-    private static List<Integer> result = new ArrayList<>();
-
+    /**
+     * Finds the shortest cycle in the given graph.
+     *
+     * @param g The input graph.
+     * @param cutoffTime cutoff time for the algorithm
+     * @param seed The seed for random selection of the starting vertex.
+     */
     public static void findShortestCycle(Graph g, int cutoffTime, int seed) {
         int dimension = g.getDimension();
 
@@ -32,6 +40,13 @@ public class LocalSearch {
         }
     }
 
+    /**
+     * Generate random tour of the graph
+     *
+     * @param dimension Dimension of the graph.
+     * @param seed random seed.
+     * @return A tour of the graph.
+     */
     private static List<Integer> generateRandomTour(int dimension, int seed) {
         List<Integer> tour = new ArrayList<>();
         for (int i = 1; i <= dimension; i++) {
@@ -41,6 +56,12 @@ public class LocalSearch {
         return tour;
     }
 
+    /**
+     * Generate neighbour tour.
+     *
+     * @param tour current tour.
+     * @return the neighbour tour.
+     */
     private static List<Integer> generateNeighborTour(List<Integer> tour) {
         int n = tour.size();
         List<Integer> neighborTour = new ArrayList<>(tour);
@@ -63,7 +84,13 @@ public class LocalSearch {
         return neighborTour;
     }
 
-
+    /**
+     * Calculate the final cost.
+     *
+     * @param g Graph.
+     * @param tour Current tour.
+     * @return Total cost of the tour.
+     */
     private static int calculateTourCost(Graph g, List<Integer> tour) {
         int cost = 0;
         for (int i = 0; i < tour.size() - 1; i++) {
@@ -78,6 +105,13 @@ public class LocalSearch {
         return cost;
     }
 
+    /**
+     * Set result to the graph.
+     *
+     * @param g Graph.
+     * @param tour Input tour.
+     * @param cost Minimum cost.
+     */
     private static void updateGraphResult(Graph g, List<Integer> tour, int cost) {
         g.setResult(tour);
         g.setMinimumCost(cost);
